fb75a9fd3927b8bb77a5fa1c18bba22f4b054911
ostosh <ostoshg@gmail.com>
2016-01-31 23:42:27 -0800

    add react-redux proto

--- src/app/component/app.jsx
+++ src/app/component/app.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
-
+import {Provider} from 'react-redux';
+import Router, {Route} from 'react-router';
 import ThemeManager from 'material-ui/lib/styles/theme-manager';
 import LightRawTheme from 'material-ui/lib/styles/raw-themes/light-raw-theme';
 import Colors from 'material-ui/lib/styles/colors';
 
-
 import AppBar from './navigation/appBar';
-import ScreenContainer from './view/start';
+import LoginScreen from './view/loginScreen';
 
+
+const routes = <Route path="/">
+  <Route path="login" component={LoginScreen} />
+  <Route path="" component={LoginScreen} />
+  {/*<NotFoundRoute />*/}
+</Route>;
 
 export default React.createClass({
   render() {
     return (
-      <div>
-        <AppBar />
-        <ScreenContainer />
-      </div>
+     <Provider store={this.props.store}>
+       <div>
+       <AppBar />
+       <Router>{routes}</Router>
+       </div>
+     </Provider>
     );
   },
 });
