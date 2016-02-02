d959d80e813cb696f8f0740e2f01eae0df8d1c18
ostosh <ostoshg@gmail.com>
2016-02-01 23:29:26 -0800

    add broadcastModal as seperate component

---
+++ src/app/component/modal/broadcastModal.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
+
+import Modal from '../container/modal';
+import BroadcastForm from '../form/broadcast';
+import ButtonMenu from '../navigation/buttonContainer';
+
+export default React.createClass({
+
+  getInitialState() {
+    return {
+      open: false,
+    };
+  },
+
+  toggleModal(){
+    this.setState({
+      open: !this.state.open,
+    });
+  },    
+
+  handleBroadcast() {
+    this.refs.broadcastForm.submit();
+  },
+
+  render() {
+  
+    let cancelMessageButton = (
+      <FlatButton style={{margin:'5px'}} label="Cancel" secondary={true} onTouchTap={this.toggleModal} />
+    );
+
+    let broadcastButton = (
+      <FlatButton style={{margin:'5px'}} label="Broadcast" secondary={true} onTouchTap={this.handleBroadcast} />
+    );
+ 
+    let broadcastMenu = (
+      <ButtonMenu actions={[cancelMessageButton, broadcastButton]} />
+    );
+
+    let broadcastForm = (
+      <BroadcastForm ref='broadcastForm' />
+    );
+
+    return (
+      <Modal open={this.state.open} title="Sign Up" content={broadcastForm} actions={broadcastMenu} />
+    );
+  },
+});
+
