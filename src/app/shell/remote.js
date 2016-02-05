6ee9c80d340b00f260dfa23b0c1b99a63ca6080f
ostosh <ostoshg@gmail.com>
2016-02-04 23:41:16 -0800

    update remote middlewear to generic middlewear spec

--- src/app/shell/remote.js
+++
@@ -1,9 +0,0 @@
-import validator from '../util/validator'
-
-export default socket => store => next => action => {
-  if (Object.isObject(action['meta']) && action['meta']['remote']) {
-    console.log('emote middleware called with action: ', action);
-    //TODO crud router api interface
-  }
-  return next(action);
-}
