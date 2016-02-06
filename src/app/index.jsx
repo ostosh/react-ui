e8dce3511de5fe852419ab484904334ec4d0d6c3
ostosh <ostoshg@gmail.com>
2016-02-05 21:04:51 -0800

    refactor component app into app entry

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,15 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import Router, {Route} from 'react-router';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
-import App from './component/app';
 import initStore from './shell/store';
+
+import NavigationBar from './component/navigation/navigationBar';
+import LoginScreen from './component/view/loginScreen';
+import BroadcastModal from './component/modal/broadcastModal';
+import SearchModal from './component/modal/searchModal';
+import SignupModal from './component/modal/signupModal';
 
 injectTapEventPlugin();
 
 const store = initStore();
 
+const App = (props) => (
+  <div>
+    <NavigationBar />
+    {props.children}
+  </div>
+)
+
+const routes = <Route path="/" component={App} >
+  <Route path="login" component={LoginScreen} />
+</Route>;
+
+
 ReactDOM.render(
-  <App store={store} />, 
+  <Provider store={store}>
+    <Router>{routes}</Router>
+  </Provider>, 
   document.getElementById('app')
 );
+
+
